55770a63d08c5942731a68e3072ba50c2487e34a
marcogiammari <marcogiammari@gmail.com>
2025-02-06 14:48:43 +0100

    feat: textnode

---
+++ src/textnode.py
@@ -0,0 +1,75 @@
+.gitignore
+A
+
+main.sh
+A
+
+src/main.py
+A
+
+src/textnode.py
+A
+
+public/index.html
+D
+
+public/styles.css
+D
+1
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+from enum import Enum
+
+
+class TextType(Enum):
+    TEXT = "text"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
+
+
+class TextNode:
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other):
+        return (
+            self.text_type == other.text_type
+            and self.text == other.text
+            and self.url == other.url
+        )
+
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
